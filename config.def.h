d26db1531d59a3fb496ddd477b0589ddd188b337
Nihan Cj <nihancj.com@gmail.com>
2022-06-06 21:12:35 +0530

    Added to github

--- config.def.h
+++ config.def.h
@@ -24,7 +24,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "7", "8", ""};
+static const char *tags[] = { "", "", "", "", "" , "", "7", "8", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
