5f8af59a5ff5df2f84724eb413b904fe6ea5542c
Nihan Cj <nihancj.com@gmail.com>
2022-06-22 19:36:56 +0530

    refactor: Replace patch attachaside w/ attachbelow

--- config.def.h
+++ config.def.h
@@ -44,6 +44,7 @@ static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int attachbelow = 1;    /* 1 means attach after the currently active window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
