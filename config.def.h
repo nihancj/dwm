d7dcfa3ec0e4bac30a99282856e702e1707fd134
Nihan Cj <nihancj.com@gmail.com>
2022-10-19 14:46:46 +0530

    Unpatch: autostart

--- config.def.h
+++ config.def.h
@@ -31,13 +31,15 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "WhatsApp Web", NULL,     NULL,      1 << 7,    0,          0,          -1,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
-	{ "Galculator", NULL,  NULL,           0,         1,          0,           0,        -1 },
+	/* class            instance    title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",           NULL,       NULL,           0,         1,          0,           0,        -1 },
+	{ "WhatsApp Web",   NULL,       NULL,           1 << 7,    0,          0,          -1,        -1 },
+	{ "Firefox",        NULL,       NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "firefox-esr",   NULL,       NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",             NULL,       NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester",          0,         0,          0,           1,        -1 }, /* xev */
+	{ "Galculator",     NULL,       NULL,           0,         1,          0,           0,        -1 },
+	{ "Dragon-drop",    NULL,       NULL,           0,         1,          0,           1,        -1 },
 };
 
 /* layout(s) */
