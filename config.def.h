23b85569245f57c86e331ee07a60802eb9bc4d1d
Nihan Cj <nihancj.com@gmail.com>
2023-03-20 21:07:33 +0530

    About Firefox now floats

--- config.def.h
+++ config.def.h
@@ -31,15 +31,16 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class            instance    title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",           NULL,       NULL,           0,         1,          0,           0,        -1 },
-	{ "WhatsApp Web",   NULL,       NULL,           1 << 7,    0,          0,          -1,        -1 },
-	{ "Firefox",        NULL,       NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "firefox-esr",   NULL,       NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",             NULL,       NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester",          0,         0,          0,           1,        -1 }, /* xev */
+	/* class            instance    title           tags mask	isfloating  isterminal  noswallow  monitor */
+	{ "firefox-esr",	NULL,       NULL,           1 << 8,		0,          0,           0,        -1 },
+	{ "WhatsApp Web",   NULL,       NULL,           1 << 7,		0,          0,          -1,        -1 },
+	{ "Gimp",           NULL,       NULL,           0,			1,          0,           0,        -1 },
+	{ "St",             NULL,       NULL,           0,			0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester",          0,			0,          0,           1,        -1 }, /* xev */
+
 	{ "Galculator",     NULL,       NULL,           0,         1,          0,           0,        -1 },
 	{ "Dragon-drop",    NULL,       NULL,           0,         1,          0,           1,        -1 },
+	{ NULL,	NULL,"About Mozilla Firefox",			0,         1,          0,           1,        -1 },
 };
 
 /* layout(s) */
